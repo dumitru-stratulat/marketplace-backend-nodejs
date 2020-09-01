9e9a18330dba22dde06ffbb2ab454c97ee0772f5
Your Name <you@example.com>
2020-09-01 17:39:27 +0300

    added routes

---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+
+const express = require('express');
+const { body } = require('express-validator/check')
+
+const User = require('../models/user');
+const authController = require('../controllers/auth')
+
+
+const router = express.Router();
+
+router.put('/signup',[
+  body('email')
+  .isEmail()
+  .withMessage('Enter valid email')
+  .custom((value,{req}) =>{
+    return User.findOne({email: value})
+    .then(userDoc=>{
+      if(userDoc){
+        return Promise.reject('E-mail addres already exists!')
+      }
+    })
+  }),
+  body('password')
+  .trim()
+  .isLength({min: 5}),
+  body('name')
+  .trim()
+  .not()
+  .isEmpty()
+],authController.signup);
+
+router.post('/login',authController.login);
+
+module.exports = router;
