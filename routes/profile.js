7b29b169a2eed9d35c2536925264ebecdab30522
Your Name <you@example.com>
2020-10-09 13:13:37 +0300

    added profile validation

--- routes/profile.js
+++ routes/profile.js
@@ -7,7 +7,38 @@ const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-router.get('/profile/:profileId', profileController.getProducts);
-router.post('/create',isAuth, profileController.createProduct);
+router.get('/profile/:profileId',profileController.getProducts);
+router.post('/create',
+[ 
+body('title')
+  .isLength({min: 2})
+  .isLength({max: 100}),
+  //for future development
+//   body('content')
+//   .isLength({min: 2})
+//   .isLength({max: 500}),
+//   body('category')
+//   .not()
+//   .isEmpty(),
+//   body('images')
+//   .not()
+//   .isEmpty(),
+//   body('condition')
+//   .not()
+//   .isEmpty(),
+//   body('size')
+//   .not()
+//   .isEmpty(),
+//   body('contactNumber')
+//   .isNumeric()
+//   .isLength({min: 5})
+//   .isLength({max: 12}),
+//   body('price')
+//   .isNumeric()
+//   .not()
+//   .isEmpty(),
+],
+isAuth,
+profileController.createProduct);
 
 module.exports = router;
